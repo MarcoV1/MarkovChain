9957499c3ee0e8c4eb4be88004acfe742609d735
Francisco Lopes <franlopt@gmail.com>
2017-10-15 23:28:00 +0100

    Small fixes on letter_probability()

--- fcm.py
+++ fcm.py
@@ -7,13 +7,13 @@ texto = ""
 # STATISTICAL INFORMATION
 
 
-def lerFicheiro(ficheiro):
+def read_file(file):
     global texto
-    with open(ficheiro, encoding="utf8") as f:
+    with open(file, encoding="utf8") as f:
         texto = f.read().upper()
         texto1 = ""
         for i in texto:
-            if i.isalpha() or i == " " or i=="\n":
+            if i.isalpha() or i == " " or i == "\n":
                 texto1 += i
         texto = texto1
     return texto1
@@ -31,7 +31,7 @@ def combinations_in_text(order):
     combinations_txt = []
     for c in range(len(texto)):
         cmb = ""
-        if(c > (len(texto)) - order):
+        if c > len(texto) - order:
             break
         for i in range(c, order + c):
             cmb += texto[i]
@@ -43,11 +43,10 @@ def combination_next_letter(order):
     global texto
 
     ocorr = dict()
-    alfab = alfabeto()
 
     for i in range(0, len(texto) - order):
-        #print("Combinação: {}, Letra seguinte: {}".format(text[i : order + i], text[order + i]))
-        cmb = texto[i : order + i]
+        # print("Combinação: {}, Letra seguinte: {}".format(text[i : order + i], text[order + i]))
+        cmb = texto[i: order + i]
         next_letter = texto[order + i]
         if cmb in ocorr:
             if next_letter in ocorr[cmb]:
@@ -55,34 +54,38 @@ def combination_next_letter(order):
             else:
                 ocorr[cmb][next_letter] = 1
         else:
-            ocorr[cmb] = {next_letter : 1}
+            ocorr[cmb] = {next_letter: 1}
     return ocorr
+
 
 def calc_ocorr(val):
     count = 0
     lista_val = list(val.values())
     for x in lista_val:
-        count+=x
+        count += x
     return count
 
+
 def letter_probability(order):
-    letter_prob={}
+    letter_prob = {}
     alfb = alfabeto()
     global alpha
     lac = combination_next_letter(order)
-    for key,value in lac.items():
+    for key, value in lac.items():
         alf1 = alfb
         counter = 0
-        alf1 = list(filter(lambda z: z not in list(value.keys()),alfb))
-        for x,v in value.items():
-            if(counter>0):
-                letter_prob[key][x] = (v+alpha) / (calc_ocorr(value) + alpha)
+        alf1 = list(filter(lambda z: z not in list(value.keys()), alfb))
+        for x, v in value.items():
+            if(counter > 0):
+                letter_prob[key][x] = (v + alpha) / (calc_ocorr(value) + len(alfb) * alpha)
             else:
-                letter_prob[key] = {x: (v+alpha) / (calc_ocorr(value) + alpha)}
+                letter_prob[key] = {x: (v + alpha) / (calc_ocorr(value) + len(alfb) * alpha)}
                 counter += 1
         for l in alf1:
             letter_prob[key][l] = (alpha) / (calc_ocorr(value) + len(alfb) * alpha)
     return letter_prob
+
+
 """
 def teste(letprob):
     soma = 0
@@ -96,23 +99,22 @@ def teste(letprob):
 
 
 def entropy_calc(letprob):
-    #print(letprob)
-    #print(alfabeto())
-    entropia_total=0
-    for k,v in letprob.items():
-        entropy=0
-        for x,y in v.items():
-            entropy+=y*math.log(y,2)
-        #print("Combinação: "+k+"    Entropia: "+str(-entropy))
-    entropia_total = -entropy
-    print("Entropia total estimada do texto: {}".format(entropia_total))
+
+    entropy = 0
+    for k, v in letprob.items():
+        for x, y in v.items():
+            entropy -= y * math.log(y, 2)
+
+    print("Entropia total estimada do texto: {}".format(entropy))
+    print("Entropia Média: {}".format(entropy/len(letprob.keys())))
     return ""
 
-order = 2 #para alterar a ordem
-ficheiro = "texto.txt" #path do ficheiro a ser lido
-alpha = 0.0000000000001#valor de alpha
-lerFicheiro(ficheiro)
-#print("1ª linha - combinações possíveis\n2ª linha combinações + prob de ocorrências de cada letra a seguir à comb.\n3ª linha alfabeto\n")
+
+order = 9  # para alterar a ordem
+ficheiro = "texto.txt"  # path do ficheiro a ser lido
+alpha = 0.0000000000001  # valor de alpha
+read_file(ficheiro)
+# print("1ª linha - combinações possíveis\n2ª linha combinações + prob de ocorrências de cada letra a seguir à comb.\n3ª linha alfabeto\n")
 
 s2 = time.time()
 lp = letter_probability(order)
--- generator.py
+++ generator.py
@@ -1,21 +1,24 @@
 from fcm import *
 import numpy as np
 import random
-generate_size = 1000 #tamanho do texto a ser gerado
-generate_file = "generated_text.txt" #ficheiro onde o texto vai ser gerado
-#para alterar as outras variáveis globais (order,etc...) altere no fcm.py
+
+generate_size = 1000  # tamanho do texto a ser gerado
+generate_file = "generated_text.txt"  # ficheiro onde o texto vai ser gerado
+# para alterar as outras variáveis globais (order,etc...) altere no fcm.py
 # --------------------- GENERATE TEXT ----------------------------------------
+
 
 def write_initial():
     global order
     while True:
         txt_input = input("Insira a combinação/frase inicial (com coerência se possível): \n").upper()
-        if(len(txt_input)<order):
+        if len(txt_input < order):
             print("Ordem demasiado grande para texto tão pequeno!\n")
             continue
         else:
             break
     return txt_input
+
 
 def generate(size):
     global lp
@@ -25,15 +28,15 @@ def generate(size):
     filtered_txt = ""
     for i in initial_text:
         if i.isalpha() or i == " ":
-            filtered_txt+= i
-    while len(filtered_txt)<size:
+            filtered_txt += i
+    while len(filtered_txt) < size:
         last_cmb = filtered_txt[-order:]
-        if(last_cmb in list(lp.keys())):
-            filtered_txt += ''.join(np.random.choice(list(lp[last_cmb].keys()),1,p=list(lp[last_cmb].values())))
+        if last_cmb in list(lp.keys()):
+            filtered_txt += ''.join(np.random.choice(list(lp[last_cmb].keys()), 1, p=list(lp[last_cmb].values())))
         else:
             print(len(filtered_txt))
             filtered_txt += random.choice(al)
-    file = open(generate_file,'w')
+    file = open(generate_file, 'w')
     file.write(filtered_txt)
     file.close()
     return "Texto gerado!"
