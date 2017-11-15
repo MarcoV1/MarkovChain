a280f1594df9b703b17db57e52c4f50cd664a9a5
Francisco Lopes <franlopt@gmail.com>
2017-11-15 20:29:48 +0000

    Accuracy

--- tests_plots/accuracy.py
+++ tests_plots/accuracy.py
@@ -1,68 +1,66 @@
 from fcm import *
 from language import bit_estimation_for_guess
-import os, re, time as tm
+import os, re
+from time import time as tm
 
-percentage = [0.05, 0.1, 0.25, 0.5, 0.75, 1]
-lang_accuracy = []
-each_lang = []
+lang_accuracy, each_lang, lang_length, lang_learn = [], [], [], []
 confusion_matrix = dict()
 order = 3
 alpha = 0.01
+tamanho = 700000
 
-total = tm.time()
+total = tm()
 
 for idioms in os.listdir(os.path.join(os.getcwd(), "..\\Idioms")):
+    print(re.sub(".txt", "", idioms).capitalize(), "model")
     text = read_file(os.path.join(os.getcwd(), "..\\Idioms", idioms))
+    text = text[0: tamanho]
     each_lang.append((re.sub(".txt", "", idioms).capitalize(), text))
+    lang_length.append((len(text), re.sub(".txt", "", idioms).capitalize()))
 
-for langs in os.listdir(os.path.join(os.getcwd(), "..\\guess_texts")):
-    lang_accuracy.append([re.sub("_guess.txt", "", langs).capitalize(), 0])
+    cnl = combination_next_letter(order, text)
+    lp = letter_probability(cnl, alpha, text)
 
-for guess in os.listdir(os.path.join(os.getcwd(), "..\\guess_texts")):
-    start = tm.time()
-    text_guess = read_file(os.path.join(os.getcwd(), "..\\guess_texts", guess))
-    language = re.sub("_guess.txt", "", guess).capitalize()
-    
+    lang_learn.append((re.sub(".txt", "", idioms).capitalize(), lp, cnl))
+
+    lang_accuracy.append([re.sub(".txt", "", idioms).capitalize(), 0])
+
+print(lang_length)
+
+for idiom in os.listdir(os.path.join(os.getcwd(), "..\\Classifier")):
+    start = tm()
+    language = idiom
     print("Começou", language)
+    for text in os.listdir(os.path.join(os.getcwd(), "..\\Classifier\\" + idiom)):
+        text_guess = read_file(os.path.join(os.getcwd(), "..\\Classifier\\" + language, text))
+        lang_bit = []
+        for data in lang_learn:
+            be = bit_estimation_for_guess(text_guess, data[1], data[2], alpha, order)
+            lang_bit.append((data[0], be))
 
-    for p1 in percentage:
-        lang_learn = []
-        for l, s in each_lang:
-            cnl = combination_next_letter(order, s[0: round(len(s) * p1)])
-            lp = letter_probability(cnl, alpha, s[0: round(len(s) * p1)])
+        lang_guess = min(lang_bit, key=lambda t: t[1])[0]
 
-            lang_learn.append((re.sub(".txt", "", l).capitalize(), lp, cnl))
-
-        for p2 in percentage:
-            lang_bit = []
-            for data in lang_learn:
-                be = bit_estimation_for_guess(text_guess[0: round(len(text_guess) * p2)], data[1], data[2], alpha, order)
-                lang_bit.append((re.sub(".txt", "", data[0]).capitalize(), be))
-
-            lang_guess = min(lang_bit, key=lambda t: t[1])[0]
-
-            if language in confusion_matrix:
-                if lang_guess in confusion_matrix[language]:
-                    confusion_matrix[language][lang_guess] += 1
-                else:
-                    confusion_matrix[language][lang_guess] = 1
+        if language in confusion_matrix:
+            if lang_guess in confusion_matrix[language]:
+                confusion_matrix[language][lang_guess] += 1
             else:
-                confusion_matrix[language] = {lang_guess: 1}
+                confusion_matrix[language][lang_guess] = 1
+        else:
+            confusion_matrix[language] = {lang_guess: 1}
 
+        if language == lang_guess:
+            for j in lang_accuracy:
+                if j[0] == language:
+                    j[1] += 1
+                    break
 
-            if language == lang_guess:
-                for j in lang_accuracy:
-                    if j[0] == language:
-                        j[1] += 1
-                        break
-    
-    print("Acabou", language, (tm.time() - start), "seg")
+    print("Acabou", language, (tm() - start), "seg")
 
-end = tm.time() - total
+end = tm() - total
     
 print(lang_accuracy)
-print([(x, y / len(percentage) ** 2) for x, y in lang_accuracy])
-print("Tempo total:", (end / 60), "minutos e ", (end % 60), "segundos")
+print([(x, y / 10) for x, y in lang_accuracy])
+print("Tempo total:", int(end / 60), "minutos e ", round(end % 60, 2), "segundos")
 
 for lang, data in confusion_matrix.items():
     print(lang)
